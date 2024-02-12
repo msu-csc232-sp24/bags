82c62cec9e2d6d24f083be509c33aecee4692d2e
Jim Daehn <jdaehn@icloud.com>
2024-02-12 10:33:28 -0600

    Update formatting in Node files

--- include/Node.h
+++ include/Node.h
@@ -1,27 +1,62 @@
-//  Created by Frank M. Carrano and Timothy M. Henry.
-//  Copyright (c) 2017 Pearson Education, Hoboken, New Jersey.
+/**
+ * CSC232 - Data Structures
+ * Missouri State University, Spring 2024
+ *
+ * Listing 4-1
+ *
+ * Created by Frank M. Carrano and Timothy M. Henry with stylistic modifications made by Jim Daehn.
+ *
+ * @file    Node.h
+ * @author  Jim Daehn <jdaehn@missouristate.edu>
+ * @brief   Macros, libraries and definitions for use in csc232 assignments.
+ * @version 0.1
+ * @date    9/10/2021
+ *
+ * @copyright 2017 Pearson Education, Hoboken, New Jersey.
+ */
 
-/** @file Node.h
-    Listing 4-1 */
 #ifndef NODE_
 #define NODE_
 
 template<class ItemType>
-class Node
-{
+class Node {
 private:
-    ItemType        item; // A data item
-    Node<ItemType>* next; // Pointer to next node
+    ItemType item; // A data item
+    Node<ItemType> *next; // Pointer to next node
 
 public:
-    Node();
-    Node(const ItemType& anItem);
-    Node(const ItemType& anItem, Node<ItemType>* nextNodePtr);
-    void setItem(const ItemType& anItem);
-    void setNext(Node<ItemType>* nextNodePtr);
-    ItemType getItem() const ;
-    Node<ItemType>* getNext() const ;
+
+    /**
+     * Initializing constructor.
+     * @param anItem the initial item encapsulated by this Node
+     */
+    explicit Node(const ItemType &anItem = ItemType{}, Node<ItemType> *nextNodePtr = nullptr);
+
+    /**
+     * Item mutator method.
+     * @param anItem an item to replace the current item in this Node
+     */
+    void setItem(const ItemType &anItem);
+
+    /**
+     * Next node pointer mutator method.
+     * @param nextNodePtr a node pointer to replace the current node pointer in this Node
+     */
+    void setNext(Node<ItemType> *nextNodePtr);
+
+    /**
+     * Item accessor method.
+     * @return a copy of the item encapsulated in this Node
+     */
+    ItemType getItem() const;
+
+    /**
+     * Next node pointer accessor method.
+     * @return a copy of the next node pointer in this Node
+     */
+    Node<ItemType> *getNext() const;
 }; // end Node
 
 #include "Node.cpp"
+
 #endif
--- src/main/cpp/Node.cpp
+++ src/main/cpp/Node.cpp
@@ -7,16 +7,6 @@
 //#include <cstddef>
 
 template<class ItemType>
-Node<ItemType>::Node() : next(nullptr)
-{
-} // end default constructor
-
-template<class ItemType>
-Node<ItemType>::Node(const ItemType& anItem) : item(anItem), next(nullptr)
-{
-} // end constructor
-
-template<class ItemType>
 Node<ItemType>::Node(const ItemType& anItem, Node<ItemType>* nextNodePtr) :
         item(anItem), next(nextNodePtr)
 {
